712dce7eeda5c836dd461186216e7a63f904d14b
Arison13 <arisonarias@gmail.com>
2022-10-30 23:13:13 -0400

    added more animation to agents in agentlist

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,15 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className='navbar'> 
-        <h4 className='navbar-left'>Welcome to Valorant</h4>
+    <div className='navbar-left'> 
+        <h4 className='nav-title'>Valorant For Noobs</h4>
+    </div>
         <ul className='navbar-right'> 
             <li> <Link to='/agents'>Agents</Link> </li>
             <li> <Link to='/weapons'>Weapons</Link> </li>
             <li> <Link to='/maps'>Maps</Link> </li>
+            <li> <Link to='/:matchID'>Look Up Match</Link> </li> 
+            {/* need to use other server to get match info based on ID */}
         </ul>
     </header>
   )
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import '../styles/LoadingScreen.css';
 
 function LoadingScreen() {
   return (
-    <div>Data Loading...</div>
+    <div className='loading-main'> 
+     <h4> Your favorite Valorant Data is Loading. 
+      <br/>Keep your crosshair ready and hold this angle for one more second.</h4>
+      {/* Put link to watch some valorant video while you wait */}
+    </div>
   )
 }
 
