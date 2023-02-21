8e6d6f8c74b2e28e78430ff0bafb7b5874f16e0e
Arison13 <arisonarias@gmail.com>
2023-02-21 01:01:57 -0500

    using tailwind to update some styles

--- src/pages/HomeDisplay/Home.jsx
+++ src/pages/HomeDisplay/Home.jsx
@@ -1,49 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import News from './News';
-import '../../styles/HomeStyles/Home.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import News from "./News";
+import "../../styles/HomeStyles/Home.css";
 
 function Home() {
-    const [esports, setEsports] = useState([])
-    const [currentSelection, setCurrentSelection] = useState('esports')
+  const [esports, setEsports] = useState([]);
+  const [currentSelection, setCurrentSelection] = useState("esports");
 
-    useEffect( ()=> {
-        axios.get(`https://api.henrikdev.xyz/valorant/v1/website/en-us?filter=${currentSelection}`)
-            .then(res => {
-                setEsports(res.data.data.slice(0, 9))
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.henrikdev.xyz/valorant/v1/website/en-us?filter=${currentSelection}`
+      )
+      .then((res) => {
+        setEsports(res.data.data.slice(0, 9));
+      })
+      .catch((err) => console.log({ err }));
+  }, [currentSelection]);
 
-            })
-            .catch(err => console.log({err}))
-    },[currentSelection])
-    
-    const handleSelection = (ev) => {
-        ev.preventDefault()
-        setCurrentSelection(ev.target.value)
-    }
+  const handleSelection = (ev) => {
+    ev.preventDefault();
+    setCurrentSelection(ev.target.value);
+  };
 
   return (
-    <div className='home-view'>
-        <div className='landing'> 
-            <h1> Welcome to Valorant for Noobs.</h1>
-            <p>We're glad to see your interest for improving at valorant</p>
-        </div>
+    <div className="home-view">
+      <div className="landing">
+        <h1> Welcome to Valorant for Noobs.</h1>
+        <p>We're glad to see your interest for improving at valorant</p>
+      </div>
 
-        <div className='news-section'> 
-        <div className='form-select'>
-                <h3>News Section</h3>
-                <form> 
-                    <select value={currentSelection} onChange={(ev)=> handleSelection(ev) }> 
-                        <option value='announcements'> Announcements</option>
-                        <option value='esports'> Esports</option>
-                        <option value='game_updates'> Game Updates </option>
-                    </select>
-                </form>
-            </div>
-            <News news={esports}/>
+      <div className="flex flex-col items-center">
+        <div id="form-select" className=" flex flex-col mt-4 gap-2">
+          <h3>News Section</h3>
+          <form>
+            <select
+              value={currentSelection}
+              onChange={(ev) => handleSelection(ev)}
+            >
+              <option value="announcements"> Announcements</option>
+              <option value="esports"> Esports</option>
+              <option value="game_updates"> Game Updates </option>
+            </select>
+          </form>
         </div>
+        <News news={esports} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
--- src/pages/MatchDisplay.jsx
+++ src/pages/MatchDisplay.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function MatchDisplay() {
-  alert("This page is currentyl being built... Check back later")
+  // alert("This page is currentyl being built... Check back later")
   return (
-    <div><h3> This page is currentyl being built... <br/> Check back later</h3></div>
-  )
+    <div className="bg-red-600">
+      <h3>
+        This page is currentyl being built... <br /> Check back later
+      </h3>
+    </div>
+  );
 }
 
-export default MatchDisplay
+export default MatchDisplay;
