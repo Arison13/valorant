488d01828d0c40f243a9233369aa003b9a61a638
Arison13 <arisonarias@gmail.com>
2023-02-22 21:28:19 -0500

    cleanig up and making code more readable and maintable

--- src/pages/searchPage/search-page.jsx
+++ src/pages/searchPage/search-page.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 // https://docs.henrikdev.xyz/valorant.html
 //docs for API thats gonna be use to power this page
 export const SearchPage = () => {
+  const SearchOptionButton = ({ text }) => {
+    return (
+      <button className="py-2 px-4 lg:w-auto w-4/6 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
+        {text}
+      </button>
+    );
+  };
   const SearchOptionsContainer = () => {
+    const [showDropDown, setShowDropDown] = useState(false);
     return (
       <div className="flex lg:flex-row flex-col w-full justify-center items-center gap-6 p-4">
-        <button className="py-2 px-4 lg:w-auto w-4/6 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
-          Search Latest Matches
-        </button>
-        <button className="py-2 px-4 lg:w-auto w-4/6 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
-          Search MMR History
-        </button>
-        <button className="py-2 px-4 lg:w-auto w-4/6 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
-          Search Player Profile
-        </button>
+        <SearchOptionButton text="Search Latest Matches" />
+        <SearchOptionButton text="Search MMR History" />
+        <SearchOptionButton text="Search Player Profile" />
       </div>
     );
   };
