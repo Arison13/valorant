5c3ad2f18cdddf45a0a32888f5f2e52dc1743130
Arison13 <arisonarias@gmail.com>
2023-02-22 21:18:15 -0500

    improving layout and adding media queries for mobile

--- src/pages/searchPage/search-page.jsx
+++ src/pages/searchPage/search-page.jsx
@@ -5,21 +5,23 @@ import React from "react";
 export const SearchPage = () => {
   return (
     <div className="flex flex-col justify-between items-center gap-2 w-full">
-      <p className="mt-4 w-1/6 p-2 shadow-md  bg-[#FF4655] font-semibold text-lg font-mono rounded-md border border-slate-100 text-center">
+      <p className="mt-4 lg:w-1/6 w-3/6 p-2 shadow-md  bg-[#0F1923] font-semibold text-lg text-[#99ABB] font-mono rounded-md text-center ">
         Search Options
       </p>
 
-      <div className="flex w-full justify-center items-center gap-6 p-4">
-        <button className="py-2 px-4 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
+      <div className="flex lg:flex-row flex-col w-full justify-center items-center gap-6 p-4">
+        <button className="py-2 px-4 lg:w-auto w-4/6 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
           Search Latest Matches
         </button>
-        <button className="py-2 px-4 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
+        <button className="py-2 px-4 lg:w-auto w-4/6 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
           Search MMR History
         </button>
-        <button className="py-2 px-4 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
+        <button className="py-2 px-4 lg:w-auto w-4/6 shadow-lg shadow-[#5589bdff] font-semibold font-mono rounded-md bg-slate-100 text-[#FF4655]">
           Search Player Profile
         </button>
       </div>
     </div>
   );
 };
+
+// border-b border-[#FF4655]
